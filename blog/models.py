949206452143227856dbf7ab259cf5c3a19bdc38
윤태영 <dhrmsry777@naver.com>
2020-07-11 16:28:35 +0900

    post :list view

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User  # User 객체 제공
 
-
+# Post는 모델의 이름입니다.
+# models은 Post가 장고 모델임을 의미합니다. 이 코드 때문에 장고는 Post가 데이터베이스에 저장되어야 한다고 알게 됩니다.
 class Post(models.Model):
     # 블로그 글 제목
     title = models.CharField(max_length=30)  # 길이제한 필요
@@ -12,3 +13,7 @@ class Post(models.Model):
     # 작성자 누군데
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     # 사용자가 삭제되었을때 글은 어떻게 할것이냐 (글도 같이 삭제)
+
+    # 제목 설정
+    def __str__(self):
+        return '{} :: {}'.format(self.title, self.author)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Post  # . = 지금 폴더안
+# post = models.py의 post가져옴
+from django.views.generic import ListView
 
+
+class PostList(ListView):
+    model = Post
+
+
+# 밑에거 필요없이 위에 2줄로 가능
+
+def get_queryset(self):
+    return Post.objects.order_by('-created')
 # Create your views here.
+
+# def index(request):
+#     posts = Post.objects.all()
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',  # templates의 html 필요
+#         {
+#             'posts': posts,  # 템플릿에 전함
+#         }
+#     )
