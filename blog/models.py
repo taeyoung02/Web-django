b07ac17273c5565fd38b74dee34c8093095944a5
윤태영 <dhrmsry777@naver.com>
2020-07-15 22:14:19 +0900

    ,

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User  # User 객체 제공
 
+
 # Post는 모델의 이름입니다.
 # models은 Post가 장고 모델임을 의미합니다. 이 코드 때문에 장고는 Post가 데이터베이스에 저장되어야 한다고 알게 됩니다.
 class Post(models.Model):
@@ -9,11 +10,12 @@ class Post(models.Model):
     # 글 내용
     content = models.TextField()
 
-    head_image=models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
+    head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
     # 언제 작성햇는데
     created = models.DateTimeField()
     # 작성자 누군데
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # 사용자가 삭제되었을때 글은 어떻게 할것이냐 (글도 같이 삭제)
 
     # 제목 설정
