4d06746dec3695e3e56099885f0e2745eb4ef6c5
윤태영 <dhrmsry777@naver.com>
2020-07-26 23:17:25 +0900

    post detail basic functions

--- blog/tests.py
+++ blog/tests.py
@@ -59,6 +59,9 @@ class Testview(TestCase):
         self.assertNotIn('아직 게시물이 없습니다.', body.text)
         self.assertIn(post_000.title, body.text)
 
+        post_000_read = body.find('a', id='read-{}'.format(post_000.pk))
+        self.assertEqual(post_000_read['href'], post_000.get_absolute_url())
+
     def test_post_detail(self):
         post_000 = create_post(
             title='The first post',
@@ -79,3 +82,11 @@ class Testview(TestCase):
         self.assertEqual(title.text, '{} - Blog'.format(post_000.title))
 
         self.check_navbar(soup)
+
+        body = soup.body
+
+        main_div = body.find('div', id = 'main_div')
+        self.assertIn(post_000.title, main_div.text)
+        self.assertIn(post_000.author.username, main_div.text)
+
+        self.assertIn(post_000.content, main_div.text)
