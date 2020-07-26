bddf81e3e8f83e325291fe6bf32ad0d66241ec09
윤태영 <dhrmsry777@naver.com>
2020-07-26 21:39:05 +0900

    post_detail

--- blog/tests.py
+++ blog/tests.py
@@ -1,27 +1,81 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from .models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # TDD
 
 
+def create_post(title, content, author):
+    blog_post = Post.objects.create(
+        title=title,
+        content=content,
+        created=timezone.now(),
+        author=author,
+    )
+    return blog_post
+
 class Testview(TestCase):
     def setUp(self):
-        self.client = Client()
+        self.client = Client()  # client가 브라우저 역할 대신
+        self.author_000 = User.objects.create(username='smith', password='no')
+
+    def check_navbar(self, soup):
+        navbar = soup.find("div", id='navbar')
+        self.assertIn("Blog", navbar.text)
+        self.assertIn("About me", navbar.text)
 
     def test_post_list(self):
         response = self.client.get('/blog/')  # Http://~~/blog의 소스를 가져오겠다
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
         soup = BeautifulSoup(response.content, 'html.parser')
-        title = soup.title
+        title = soup.title  # soup = html 태그 가져옴
 
         self.assertEqual(title.text, 'Blog')
 
-        navbar = soup.find('div', id='navbar')
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.check_navbar(soup)
+        # navbar = soup.find("div", id='navbar')
+        # self.assertIn("Blog", navbar.text)
+        # self.assertIn("About me", navbar.text)
 
         self.assertEqual(Post.objects.count(), 0)
-        self.assertIn('아직 게시물이 없습니다.', soup.body)
+        self.assertIn("아직 게시물이 없습니다", soup.body.text)
+
+        post_000 = create_post(
+            title='The first post',
+            content='hello wolrd.',
+            author=self.author_000,
+        )
 
         self.assertGreater(Post.objects.count(), 0)
+
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        body = soup.body
+        self.assertNotIn('아직 게시물이 없습니다.', body.text)
+        self.assertIn(post_000.title, body.text)
+
+    def test_post_detail(self):
+        post_000 = create_post(
+            title='The first post',
+            content='hello wolrd.',
+            author=self.author_000,
+        )
+
+        self.assertGreater(Post.objects.count(), 0)
+        post_000_url = post_000.get_absolute_url()
+        self.assertEqual(post_000_url, '/blog/{}/'.format(post_000.pk))
+
+        response = self.client.get(post_000_url)  # post_000.get_absolute_url()의 소스를 가져오겠다
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title  # soup = html 태그 가져옴
+
+        self.assertEqual(title.text, '{} - Blog'.format(post_000.title))
+
+        self.check_navbar(soup)
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('<int:pk>/', views.PostDetail.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),  # primary key
     path('', views.PostList.as_view()),
 ]
